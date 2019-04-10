70895a543ba95571306d56695daaa8c1433cebf8
Carolyn <carolynwk@gmail.com>
2019-04-10 15:06:36 -0700

    Created working Yelp API call

--- assets/script/yelp.js
+++ assets/script/yelp.js
@@ -1,10 +1,47 @@
-$.ajaxPrefilter(function(options) {
-    if (options.crossDomain && $.support.cors) {
-        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+function searchYelp() {
+    
+
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972";
+    var apiKey = "8tYUenwnc30zfZ_BU_6dIkyQM6X8MI1S9hGxquW7h0EtrBfG2vuhDsQNXqItzVm4822tyG6DZ_v-m0-H31za-2yCALyGz7A72nn3Tk95fMg7U_vouW72kaFg8wmsXHYx" 
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        headers: {
+            "accept": "application/json",
+            "x-requested-with": "xmlhttprequest",
+            "Access-Control-Allow-Origin":"*",
+            "Authorization": `Bearer ${apiKey}`
+         }
+     }).then(function(res) {
+         var results = res.businesses
+         for (let idx = 0; idx < results.length; idx++) {
+                var respData = results[idx]
+        
+                // Creating a div to hold the gifs
+                var gifDiv = $("<div class='gifs'>").attr("data-state", "still");
+                
+                // Retrieving the URL for the image
+                var imgURL = respData.image_url;
+                
+                // Creating an element to hold the still image
+                var image = $("<img>").attr("src", imgURL);
+               
+                // Appending the image
+                gifDiv.append(image);
+                
+                // Storing the rating data
+                var rating = respData.rating;
+                
+                // Creating an element to have the rating displayed
+                var pRating = $("<p class='rated'>").text("Rated: " + rating);
+                
+                // Displaying the rating
+                gifDiv.append(pRating);
+                
+                // Adding new gifs to the top of the display
+                $("#restaurants").prepend(gifDiv);
+
+     }});
+
     }
-});
-
-// IDEA: Use cors-anywhere with jQuery (yep)
-// DOCUMENTATION: http://api.jquery.com/jquery.ajaxprefilter/
-// MORE DOCUMENTATION: https://github.com/Rob--W/cors-anywhere/#documentation
-
