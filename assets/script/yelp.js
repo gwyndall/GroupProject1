65b3c932263cfcf3c65e417359cde38cfb050bdd
Carolyn <carolynwk@gmail.com>
2019-04-12 12:43:40 -0700

    Deleted extra images and worked on Yelp Rating feature

--- assets/script/yelp.js
+++ assets/script/yelp.js
@@ -19,7 +19,7 @@ function searchYelp() {
                 var respData = results[idx]
         
                 // Creating a div to hold the gifs
-                var gifDiv = $("<div class='gifs'>").attr("data-state", "still");
+                var restCard = $("<div class='cards'>");
                 
                 // Retrieving the URL for the image
                 var imgURL = respData.image_url;
@@ -28,22 +28,72 @@ function searchYelp() {
                 var image = $("<img>").attr("src", imgURL).attr("width", "200px");
                
                 // Appending the image
-                gifDiv.append(image);
+                restCard.append(image);
                 
                 var busName = respData.name;
                 var nameField = $("<p class='rated'>").text(busName);
                 // Storing the rating data
                 var rating = respData.rating;
-                
+                console.log(rating);
+
+                rateImg = starRtg(rating);
+                starUrl = "../images/yelp_stars/"+ rateImg;
+                console.log(rateImg);
                 // Creating an element to have the rating displayed
-                var pRating = $("<p class='rated'>").text("Rated: " + rating);
+                var pRating = $("<p class='rated'>").html("<img src='"+ starUrl +"'>");
                 
-                // Displaying the rating
-                gifDiv.append(nameField).append(pRating);
+                // Displaying the name and rating
+                restCard.append(nameField).append(pRating);
                 
-                // Adding new gifs to the top of the display
-                $("#restaurants").prepend(gifDiv);
+                // Adding restaurants to the display
+                $("#restaurants").append(restCard);
 
-     }});
+                // Get Rating Image
+                function starRtg(rating) {
+                        //just trying a couple lines to see if I can get it to work
+                        let rateImgFile =""
+                        if (rating = 4) {
+                            rateImgFile = "small_4.png";
+                        } else {
+                            rateImgFile = "small_0.png";
+                        };
+                        return rateImgFile;
+            
+                        // switch (rate) {
+                        //     case 1:
+                        //         "small_1.png";
+                        //         break;
+                        //     case 1.5:
+                        //         "small_1_half.png";
+                        //         break;
+                        //     case 2:
+                        //         "small_2.png";
+                        //         break;
+                        //     case 2.5:
+                        //         "small_2_half.png";
+                        //         break;
+                        //     case 3:
+                        //         "small_3.png";
+                        //         break;
+                        //     case 3.5:
+                        //         "small_3_half.png";
+                        //         break;
+                        //     case 4:
+                        //         "small_4.png";
+                        //         break;
+                        //     case "4.5":
+                        //         rate = "small_4_half.png";
+                        //         break;
+                        //     case 5:
+                        //         "small_5.png";
+                        //         break;
+                        //     default:
+                        //         "small_0.png";
+                        //         return rate;
+                        // }
+                    }
+                }
+     });
 
-    }
+
+};
