8f79cefe7f6dae6e1c23ed98919172e3a87f68f1
Rob Conway <robconway12@gmail.com>
2019-04-17 19:22:34 -0700

    started Firebase

--- assets/script/main.js
+++ assets/script/main.js
@@ -14,6 +14,8 @@ $(document).ready(function () {
         youTubeAPIkey: "AIzaSyCKMpw2nmPnon_gkh4EIXnbiAmrZNw-v4M"
     }
 
+    database.ref().push(movieAndDinnerObject);
+
     let movieData = {
         movieName: "",
         theaters: []
